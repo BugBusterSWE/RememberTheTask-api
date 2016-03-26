5dfaa6434ec94a3fd5e72db761c885ce09df846c
Davide Polonio <poloniodavide@gmail.com>
2016-03-26 11:16:30 +0100

    First package configuration

---
+++ bin/wwwrtt.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by davide on 26/03/16.
+ */
+
+import test = require('../lib/main');
+
+test.HelloWorld();
--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,8 @@
 /**
  * Created by davide on 26/03/16.
  */
+
+export function HelloWorld(): void{
+    
+    console.log("Hello World");
+}
